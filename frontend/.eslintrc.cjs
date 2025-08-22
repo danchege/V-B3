189e55d540d7f79579b9f98e872d797142514cb0
danchege <danychege28@gmail.com>
2025-08-22 17:38:36 +0300

    Fixes

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,41 +1,26 @@
-// Working ESLint configuration with all necessary plugins and rules
 'use strict';
 
 module.exports = {
-  languageOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    globals: {
-      // Browser globals
-      window: 'readonly',
-      document: 'readonly',
-      navigator: 'readonly',
-      // Node.js globals
-      process: 'readonly',
-      __dirname: 'readonly',
-      // ES2021 globals
-      Promise: 'readonly',
-      Set: 'readonly',
-    },
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
   },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
   ],
   parser: '@babel/eslint-parser',
   parserOptions: {
-    ecmaVersion: 12,
+    ecmaVersion: 'latest',
     sourceType: 'module',
     requireConfigFile: false,
-    babelOptions: {
-      presets: ['@babel/preset-react'],
-    },
     ecmaFeatures: {
       jsx: true,
     },
   },
-  plugins: ['react', 'react-hooks', 'react-refresh'],
+  plugins: ['react'],
   settings: {
     react: {
       version: 'detect',
@@ -43,16 +28,6 @@ module.exports = {
   },
   rules: {
     'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off',
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-    'no-unused-vars': ['warn', { 
-      argsIgnorePattern: '^_', 
-      varsIgnorePattern: '^_',
-      caughtErrorsIgnorePattern: '^_',
-      destructuredArrayIgnorePattern: '^_',
-    }],
   },
   ignorePatterns: [
     'node_modules',
@@ -60,13 +35,5 @@ module.exports = {
     '*.config.js',
     '*.config.cjs',
     'vite.config.js',
-    'postcss.config.js',
-    'tailwind.config.js',
-    '**/*.test.js',
-    '**/*.spec.js',
-    '**/__tests__/**',
-    '**/__mocks__/**',
-    '**/test/**',
-    '**/coverage/**',
   ],
 };
