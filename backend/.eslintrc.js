9c05d76c5cb70bce93416c001c2b799496c3c2bd
danchege <danychege28@gmail.com>
2025-08-23 17:05:01 +0300

    deploy 4

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -13,7 +13,22 @@ module.exports = {
     'no-console': 'off',
     'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
     'no-underscore-dangle': ['error', { allow: ['_id'] }],
-    'no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+    'no-unused-vars': ['error', { 
+      argsIgnorePattern: '^_', 
+      varsIgnorePattern: '^_',
+      ignoreRestSiblings: true 
+    }],
+    'consistent-return': 'off',
+    'object-shorthand': 'off',
+    'import/order': 'off',
+    'no-await-in-loop': 'off',
+    'no-plusplus': 'off',
+    'no-param-reassign': 'off',
+    'no-restricted-syntax': 'off',
+    'func-names': 'off',
+    'global-require': 'off',
+    'import/newline-after-import': 'off',
+    'no-use-before-define': 'off',
   },
   ignorePatterns: ['node_modules/', 'dist/'],
 };
